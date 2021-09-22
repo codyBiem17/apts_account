0a2a2d8ebd6d8f3eacc91b43e949b54b0899773d
biemislam <bellomaryam@ymail.com>
2021-09-22 23:21:28 +0100

    order-page features

--- src/components/order.js
+++ src/components/order.js
@@ -7,64 +7,74 @@ import { Container, Row, Col, Table } from "reactstrap";
 
 const OrderSummaryPage = () => {
     const [userOrderDetails, setUserOrderDetails] = useState({})
+    const [loading, setLoading] = useState(true)
 
     useEffect(()=>{
-        const getUserOrderDetails = async () => {
+        const getUserOrder = async () => {
             try{
                 const url = 'https://indapi.kumba.io/webdev/assignment'
-                const userOrderDetails = await axios.get(url)
+                const userOrder = await axios.get(url)
                 // console.log(userData.data.user.likes[2])
-                setUserOrderDetails(userOrderDetails.data)
+                setUserOrderDetails(userOrder.data.user)
+                setLoading(false)
             }
             catch (err){
                 console.log(err)
             }
         }
-        getUserOrderDetails()
+        getUserOrder()
     }, [])
 
     return (
-        <Container fluid={true} className="">
-            <Row>
-                <Col xs="12">
-                    <h4>Your Order has been Confirmed!</h4>
-                    <p>Hi {userOrderDetails.user.name}, </p>
-                    <p>
-                        Thank you for your patronage. Your order has been Confirmed
-                        and will be shipping soon
-                    </p>
-                </Col>
-                <hr />
-                <Col xs="12">
-                    <p>Check Order Details</p>
-                    <Table borderless>
-                        <thead>
-                            <tr>
-                            <th>Order Id</th>
-                            <th>Order Date</th>
-                            <th>Name</th>
-                            <th>Address</th>
-                            <th>Phone</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <th scope="row">1</th>
-                            <td>{userOrderDetails.order_id}</td>
-                            <td>22 Sept, 2021</td>
-                            <td>{userOrderDetails.user.name}</td>
-                            <td>{userOrderDetails.user.address}</td>
-                            <td>{userOrderDetails.user.phone}</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </Col>
-                <hr />
-                <Col xs="12">
-                    <p>Item Details</p>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            {
+                loading ? 'loading...' :
+                <Container fluid={true} className="">
+                    <Row>
+                        <Col xs="12">
+                            <h5>Your Order has been Confirmed!</h5>
+                            <p>Hi {userOrderDetails.name}, </p>
+                            <p>
+                                Thank you for your patronage. Your order has been Confirmed
+                                and will be shipping soon
+                            </p>
+                        </Col>
+                        <hr />
+                        <Col xs="12">
+                            <p>Check Order Details</p>
+                            <Table borderless>
+                                <thead>
+                                    <tr>
+                                    <th>Order Id</th>
+                                    <th>Order Date</th>
+                                    <th>Name</th>
+                                    <th>Address</th>
+                                    <th>Phone</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row">1</th>
+                                    <td>{userOrderDetails.order_id}</td>
+                                    <td>22 Sept, 2021</td>
+                                    <td>{userOrderDetails.name}</td>
+                                    <td>{userOrderDetails.address}</td>
+                                    <td>{userOrderDetails.phone}</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+                        <hr />
+                        <Col xs="12">
+                            <p>Item Details</p>
+                            <Table>
+                                
+                            </Table>
+                        </Col>
+                    </Row>
+                </Container>
+            }
+        </>
     )
 }
 
--- src/components/profile.js
+++ src/components/profile.js
@@ -7,15 +7,17 @@ import { LocationMarkerIcon } from '@heroicons/react/solid'
 
 
 const Profile = () => {
-    const [user, setUser] = useState({})
+    const [userDetails, setUserDetails] = useState({})
+    const [loading, setLoading] = useState(true)
 
     useEffect(()=>{
         const getUser = async () => {
             try{
                 const url = 'https://indapi.kumba.io/webdev/assignment'
                 const userData = await axios.get(url)
-                // console.log(userData.data.user.likes[2])
-                setUser(userData.data.user)
+                const getUserDetails = userData.data.user
+                setUserDetails(getUserDetails)
+                setLoading(false)
             }
             catch (err){
                 console.log(err)
@@ -25,78 +27,71 @@ const Profile = () => {
     }, [])
 
     return (
-        <Container fluid={true} className="profile-container">
-            <Row className="profile-header">  
-                <Col xs="12">
-                    <img src={ProfileImage} alt="user-profile-display" />
-                </Col>
-                <Col xs="12">
-                    <h4> Welcome, <span> {user.name} - </span>  </h4>
-                    <span> {user.id} </span>
-                    <p>{user.about}</p>
-                </Col>
-            </Row>
-            <Row className="h5 likes-and-dislikes">
-                <Col xs="12">
-                    My Likes are:
-                    <List>
-                        <ul>
-                            <li>
-                                {user.likes[0]}
-                            </li>
-                            <li>
-                                {user.likes[1]}
-                            </li>
-                            <li>
-                                {user.likes[2]}
-                            </li>
-                        </ul>
-                    </List>
-                </Col>
-                <Col xs="12">
-                    My Dislikes are:
-                    <List>
-                        <ul>
-                            <li>
-                                {user.likes[0]}
-                            </li>
-                            <li>
-                                {user.likes[1]}
-                            </li>
-                            <li>
-                                {user.likes[2]}
-                            </li>
-                        </ul>
-                    </List>
-                </Col>
-            </Row>
-            <Row className="user-details">
-                <Col xs="12">
-                    <Row>
-                        <Col className="h5" xs="12">
-                            Contact Information
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs="1">
-                            <FontAwesomeIcon icon="mobile-alt" /> 
-                            
-                        </Col>
-                        <Col xs="11">
-                            {user.phone}
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs="1">
-                            <LocationMarkerIcon className="location-icon" /> 
-                        </Col>
-                        <Col xs="11">
-                            {user.address}
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            {
+                loading ? 'loading...' :
+                <Container fluid={true} className="profile-container">
+                <Row className="profile-header">  
+                    <Col xs="12">
+                        <img src={ProfileImage} alt="user-profile-display" />
+                    </Col>
+                    <Col xs="12">
+                        <h4> Welcome, <span> {userDetails.name} - </span>  </h4>
+                        <span> {userDetails.id} </span>
+                        <p>{userDetails.about}</p>
+                    </Col>
+                </Row>
+                <Row className="h5 likes-and-dislikes">
+                    <Col xs="12">
+                        My Likes are:
+                        <List>
+                            <ul>
+                                <li>{userDetails.likes[0]}</li>
+                                <li>{userDetails.likes[1]}</li>
+                                <li>{userDetails.likes[2]}</li>
+                            </ul>
+                        </List>
+                    </Col>
+                    <Col xs="12">
+                        My Dislikes are:
+                        <List>
+                            <ul>
+                                <li>{userDetails.dislikes[0]}</li>
+                                <li>{userDetails.dislikes[1]}</li>
+                                <li>{userDetails.dislikes[2]}</li>
+                            </ul>
+                        </List>
+                    </Col>
+                </Row>
+                <Row className="user-details">
+                    <Col xs="12">
+                        <Row>
+                            <Col className="h5" xs="12">
+                                Contact Information
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs="1">
+                                <FontAwesomeIcon icon="mobile-alt" /> 
+                                
+                            </Col>
+                            <Col xs="11">
+                                {userDetails.phone}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs="1">
+                                <LocationMarkerIcon className="location-icon" /> 
+                            </Col>
+                            <Col xs="11">
+                                {userDetails.address}
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+            }
+        </>
     )
 }
 
